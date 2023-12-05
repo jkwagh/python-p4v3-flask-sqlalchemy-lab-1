43c5ad661a80bce7c821e0bf76375eef597ac9be
Jeremy <kwagh.jeremiah@gmail.com>
2023-12-05 16:25:57 -0500

    submit assignment

--- server/app.py
+++ server/app.py
@@ -3,7 +3,6 @@
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
@@ -21,7 +20,33 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        body = {'id': earthquake.id,
+                'location': earthquake.location,
+                'magnitude': earthquake.magnitude,
+                'year': earthquake.year
+                }
+        status = 200
+    else:
+        body = {'message': f"Earthquake {id} not found."}
+        status = 404
+    
+    return make_response(body, status)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_minimum_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    response_body = {
+        'count': len(earthquakes),
+        'quakes': [quake.to_dict() for quake in earthquakes]
+    }
+    
+    return make_response(response_body, 200)
+        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
